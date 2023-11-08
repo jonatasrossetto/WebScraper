3a3196830ffe5362277e3e0d49ab3a6c6d18d75e
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-11-08 13:29:48 -0300

    implemented the extraction of each product rating stars

--- requestData.js
+++ requestData.js
@@ -14,32 +14,49 @@ const fs = require('fs');
 // );
 
 fs.readFile('result.html', 'utf8', function (err, data) {
-  var $ = cheerio.load(data);
-  var _listOfProductIds = [];
+  let $ = cheerio.load(data);
+
+  let _listOfProductIds = [];
   $('div[data-asin]').each(function (i, elem) {
     _listOfProductIds.push($(this).attr('data-asin'));
   });
-  var _listOfProductDivs = [];
+
+  let _listOfProductDivs = [];
+  let _listOfRawProductNames = [];
+  let _listOfProductStars = [];
+
   $('div[data-asin]').each(function (i, elem) {
     _listOfProductDivs.push($(this).html());
+
     let $$ = cheerio.load($(this).html());
     let beginIndex = $$.html().indexOf(
       '<span class="a-size-base-plus a-color-base a-text-normal">'
     );
     if (beginIndex > 0) {
+      beginIndex += 58;
       let endIndex =
         beginIndex +
-        58 +
-        $$.html()
-          .substring(beginIndex + 58, $$.html().length)
-          .indexOf('</span>');
-      //   console.log(inicio, fim);
-      let rawProductName = $$.html().substring(beginIndex + 58, endIndex);
-      console.log(rawProductName);
+        $$.html().substring(beginIndex, $$.html().length).indexOf('</span>');
+      let rawProductName = $$.html()
+        .substring(beginIndex, endIndex)
+        .replace(/\n/g, '');
+      _listOfRawProductNames.push(removeUnnecessaryWhiteSpaces(rawProductName));
+      //   console.log(`[${i}]: ${rawProductName}`);
+
+      let productStars = searchAndExtractInnerText(
+        $$.html(),
+        '<span class="a-icon-alt">',
+        '</span>'
+      );
+      _listOfProductStars.push(
+        removeUnnecessaryWhiteSpaces(productStars.replace(/\n/g, ''))
+      );
     }
   });
+
   //   console.log(_listOfProductDivs[1]);
-  _listOfProductDivs;
+  //   console.log(_listOfRawProductNames);
+  console.log(_listOfProductStars);
 });
 
 async function getHTML(productURL) {
@@ -54,4 +71,52 @@ async function getHTML(productURL) {
       console.log(error);
     });
   return html;
+}
+
+function removeBeginingWhiteSpace(text) {
+  let beginIndex = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] != ' ') {
+      beginIndex = i;
+      break;
+    }
+  }
+  return text.substring(beginIndex, text.length);
+}
+
+function removeEndWhiteSpace(text) {
+  let endIndex = 0;
+  for (let i = text.length - 1; i >= 0; i--) {
+    if (text[i] != ' ') {
+      endIndex = i;
+      break;
+    }
+  }
+  return text.substring(0, endIndex + 1);
+}
+
+function removeUnnecessaryWhiteSpaces(text) {
+  let result = '';
+  let isSpace = false;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == ' ') {
+      if (!isSpace) {
+        result += text[i];
+        isSpace = true;
+      }
+    } else {
+      result += text[i];
+      isSpace = false;
+    }
+  }
+  return result;
+}
+
+function searchAndExtractInnerText(text, beginDelimiter, endDelimiter) {
+  let beginIndex = text.indexOf(beginDelimiter);
+  if (beginIndex < 0) return null;
+  beginIndex += beginDelimiter.length;
+  let endIndex =
+    beginIndex + text.substring(beginIndex, text.length).indexOf(endDelimiter);
+  return text.substring(beginIndex, endIndex);
 }
